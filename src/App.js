6795d0af70eefea664f2cd6bbf87d03053cd976d
Priscilla Yap <pyap22@gmail.com>
2019-06-08 23:49:20 +1200

    react-journal-01

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-    return ( < div className = "App" > < header className = "App-header" > < img src = {
-            logo
-        }
-        className = "App-logo"
-        alt = "logo" / > < p > Yes < /p> < a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" > Learn React < /a> < /header> < /div>);
+import React, { Component } from 'react';
+
+// import './App.css';
+import './css/materialize.css';
+import Navbar from'./components/Navbar';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
+
+class App extends Component {
+	  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+           
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/> 
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+
+
+             </div>
+       </BrowserRouter>
+      
+    );
+}
 }
 export default App;
